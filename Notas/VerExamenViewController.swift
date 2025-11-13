8a7160b1112f0c5c258ea22059f160cf655ecf8b
alumno3 <villamario7@digitechfp.com>
2025-11-13 19:10:48 +0100

    update

---
+++ Notas/VerExamenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerExamenViewController.swift
+//  Notas
+//
+//  Created by alumno3 on 12/11/25.
+//
+
+import UIKit
+
+class VerExamenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Notas/models/ExamenModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExamenModel.swift
+//  Notas
+//
+//  Created by alumno3 on 12/11/25.
+//
+
+import Foundation
